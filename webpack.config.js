4ea2f67f4df34a3f0f137919753af44f23e726c1
Anakin <anakin@admins-iMac.local>
2018-03-06 11:17:31 +0200

    Whatever

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
-
+const path = require('path')
 
 module.exports = {
-    entry: ''
-    moodules: [
-        {
-            
-        }
-    ]
+    entry: {
+        js: [
+            'babel-polyfill', path.join( __dirname, 'src/index.js'  )
+        ]
+    },
+    mode: 'development',
+    target: 'web',
+    output: {
+        path: path.join( __dirname, 'dist' ),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /.js/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            }
+        ]
+    }
 }
